705dacf4abeca091246aa5272964a919959e49dc
nvaldebenito <nvaldebenito@inti.dgf.uchile.cl>
2019-04-16 18:31:31 -0400

    edit route root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'page#index'
+  root 'eto#index'
 
-  get 'eto/index'
+  get 'page/index'
   
   get 'page/contornos', :defaults => { :format => 'json' }
   get 'page/geojson', :defaults => { :format => 'json' }
